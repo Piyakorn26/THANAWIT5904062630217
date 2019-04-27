0cd81da2672a09621436ae8ae9896727f34f02f8
unknown <agrity2009@gmail.com>
2019-04-27 23:11:20 +0700

    Updates

---
+++ precache-manifest.ac1e7ffe646c52962cbf9d3fe62dfac7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2437597095e84406b62e5b8667f3231",
+    "url": "/THANAWIT5904062630217/static/media/logo.c2437597.png"
+  },
+  {
+    "revision": "9314f64b300921739f8c49987360a2a7",
+    "url": "/THANAWIT5904062630217/static/media/homepage.9314f64b.jpg"
+  },
+  {
+    "revision": "4384fe3aa93da44476d21b8e3f7f1a92",
+    "url": "/THANAWIT5904062630217/static/media/Banner.4384fe3a.jpg"
+  },
+  {
+    "revision": "2e5ed13e19fc2d26000f",
+    "url": "/THANAWIT5904062630217/static/js/runtime~main.1088b7f1.js"
+  },
+  {
+    "revision": "05993f1db92c3e746ce3",
+    "url": "/THANAWIT5904062630217/static/js/main.e7081a0f.chunk.js"
+  },
+  {
+    "revision": "f98dc74591a06ee6303e",
+    "url": "/THANAWIT5904062630217/static/js/2.83cff44e.chunk.js"
+  },
+  {
+    "revision": "05993f1db92c3e746ce3",
+    "url": "/THANAWIT5904062630217/static/css/main.f6c83b4d.chunk.css"
+  },
+  {
+    "revision": "f98dc74591a06ee6303e",
+    "url": "/THANAWIT5904062630217/static/css/2.cf0f9f85.chunk.css"
+  },
+  {
+    "revision": "d60241f316bc675b03a6b3909a0ff248",
+    "url": "/THANAWIT5904062630217/index.html"
+  }
+];
